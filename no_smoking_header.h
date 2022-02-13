c6fbec2e9527171cb2cf083716946bb8aa1fea16
kino46danila <47347152+kino46danila@users.noreply.github.com>
2022-02-13 10:28:39 +0300

    Create no_smoking_header.h

---
+++ no_smoking_header.h
@@ -0,0 +1,22 @@
+/* Заголовочный файл должен находиться /usr/include/ */ 
+#include <iostream>                                            
+#include <ctime>
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[1;30m"
+#define RED     "\033[1;31m"
+#define GREEN 	"\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE 	"\033[1;34m"
+#define MAGENTA	"\033[1;35m"
+#define CYAN    "\033[1;36m"
+#define GREY    "\033[1;37m"
+
+#define BACK_BLAC_COLOR    "\033[1;40m"
+#define BACK_RED_COLOR     "\033[1;41m"
+#define BACK_GREEN_COLOR   "\033[1;42m"
+#define BACK_YELLOW_COLOR  "\033[1;43m"
+#define BACK_BLUE_COLOR    "\033[1;44m"
+#define BACK_MAGENTA_COLOR "\033[1;45m"
+#define BACK_CYAN_COLOR    "\033[1;46m"
+#define BACK_GREY_COLOR    "\033[1;47m"
